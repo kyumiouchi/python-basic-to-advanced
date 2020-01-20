d62674e305ea57f73fc1d667fc6f7de0bc5b67ac
yumi <yumiouchi@gmail.com>
2020-01-20 09:42:02 -0400

    11.76 - Try, Except, Else and Finally

---
+++ 11.76. Try, Except, Else and Finally.py
@@ -0,0 +1,47 @@
+"""
+Try, Except, Else and Finally
+
+Example:
+    - All input mus be treat Exception
+"""
+
+# Else -> Execute if not error happens
+
+number = 0
+try:
+    number = int(input('Write the code:'))
+except ValueError:
+    print(f'Value error')
+else:
+    print(f'You taped {number}')
+
+# Finally -> close or move resources
+
+number = 0
+try:
+    number = int(input('Write the code:'))
+except ValueError:
+    print(f'Value error')
+else:
+    print(f'You taped {number}')
+finally:
+    print('Execute finally')
+
+
+# Complete sample
+
+def divider(a, b):
+    try:
+        return int(a) / int(b)
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return 'Divider of zero'
+    except:
+        return 'Unknown error'
+
+
+num1 = input('First number: ')
+num2 = input('Second number: ')
+
+print(divider(num1, num2))
